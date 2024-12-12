80a141c7e75ca9b42191d6631ca25aea4d57be0b
Fausto Neves Kina <fausto.kina@cromai.com>
2024-12-12 16:24:05 -0300

    versionCode bump to 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.fausto.cats"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
+        versionCode = 5
         versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
